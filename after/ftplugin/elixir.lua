b205f46514fd3d17523ea32f4788d26a1bbf66f2
trbKnl <ncdeschipper@gmail.com>
2023-02-25 20:12:39 +0100

    added function to python repl plugin, added a elixir specific command to send the word end to iex

--- after/ftplugin/elixir.lua
+++ after/ftplugin/elixir.lua
@@ -1,5 +1,13 @@
+-- Elixir specific vim options
 vim.opt.number = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.expandtab = true
+
+-- See: pythonrepl-vimscript.lua
+local silent = { silent = true, noremap = true }
+local map = vim.api.nvim_set_keymap
+
+-- Send "end" to terminal buffer
+map("n", "<C-E>", ":call SendStringTermBuf('end')<CR>", silent)
