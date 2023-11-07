37f3148392cd01deb66d4d2830a71ee089a47fe1
trbKnl <ncdeschipper@gmail.com>
2023-11-07 19:34:10 +0100

    packer to lazy.vim

--- init.lua
+++ init.lua
@@ -1,6 +1,48 @@
 vim.g.mapleader = ","
 
-require("packer-plugins")
+-- initialze lazy.nvim (plugin manager)
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    "ellisonleao/gruvbox.nvim",
+    "neovim/nvim-lspconfig",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/nvim-cmp",
+    "tpope/vim-surround",
+    "tpope/vim-repeat",
+    {
+        "nvim-treesitter/nvim-treesitter", 
+        build = ":TSUpdate"
+    },
+    {
+        "nvim-tree/nvim-tree.lua",
+        version = "*",
+        lazy = false,
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        }
+    },
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.4',
+        dependencies = { 
+            'nvim-lua/plenary.nvim' 
+        }
+    }
+})
 
 -- Vim options
 local opt = vim.opt
--- plugin/gruvbox.lua
+++ plugin/gruvbox.lua
@@ -1,31 +1,27 @@
 vim.o.background = "dark" -- or "light" for light mode
-vim.cmd([[colorscheme gruvbox]])
 
--- YOU CAN MAKE CHANGES TO THE SCHEME HERE:
---
--- Default options:
---require("gruvbox").setup({
---  terminal_colors = true, -- add neovim terminal colors
---  undercurl = true,
---  underline = true,
---  bold = true,
---  italic = {
---    strings = true,
---    emphasis = true,
---    comments = true,
---    operators = false,
---    folds = true,
---  },
---  strikethrough = true,
---  invert_selection = false,
---  invert_signs = false,
---  invert_tabline = false,
---  invert_intend_guides = false,
---  inverse = true, -- invert background for search, diffs, statuslines and errors
---  contrast = "", -- can be "hard", "soft" or empty string
---  palette_overrides = {},
---  overrides = {},
---  dim_inactive = false,
---  transparent_mode = false,
---})
---vim.cmd("colorscheme gruvbox")
+require("gruvbox").setup({
+  terminal_colors = true, -- add neovim terminal colors
+  undercurl = true,
+  underline = true,
+  bold = true,
+  italic = {
+    strings = false,
+    emphasis = false,
+    comments = false,
+    operators = false,
+    folds = false,
+  },
+  strikethrough = true,
+  invert_selection = false,
+  invert_signs = false,
+  invert_tabline = false,
+  invert_intend_guides = false,
+  inverse = true, -- invert background for search, diffs, statuslines and errors
+  contrast = "", -- can be "hard", "soft" or empty string
+  palette_overrides = {},
+  overrides = {},
+  dim_inactive = false,
+  transparent_mode = false,
+})
+vim.cmd("colorscheme gruvbox")
